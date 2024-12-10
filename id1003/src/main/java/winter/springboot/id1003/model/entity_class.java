52bda9e0537793121c0498019a505913f29e99c7
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-12-10 16:17:23 +0530

    Add files via upload

--- id1003/src/main/java/winter/springboot/id1003/model/entity_class.java
+++ id1003/src/main/java/winter/springboot/id1003/model/entity_class.java
@@ -1,7 +1,12 @@
 package winter.springboot.id1003.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,4 +25,10 @@ public class entity_class {
     private double openprice;
     @JsonProperty("c")
     private double closeprice;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+    
+
 }
