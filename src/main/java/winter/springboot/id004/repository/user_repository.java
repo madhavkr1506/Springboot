9328e1474ea661261ec87bd7430571e083e5a652
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-12-11 23:56:06 +0530

    Delete src directory

--- src/main/java/winter/springboot/id004/repository/user_repository.java
+++
@@ -1,9 +0,0 @@
-package winter.springboot.id004.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import winter.springboot.id004.model.entity_class;
-
-public interface user_repository extends JpaRepository<entity_class,Long>{
-
-}
